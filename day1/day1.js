b6fcc762de772c03607f271f53b311ecf0be216b
karine <gulkarine91@gmail.com>
2024-12-02 13:16:06 +0100

    chore: finish d1

---
+++ day1/day1.js
@@ -0,0 +1,35 @@
+const fs = require("node:fs");
+
+const getTotalDistance = () => {
+  fs.readFile("./input.txt", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+
+    const lines = data.split("\n");
+
+    const list1 = [];
+    const list2 = [];
+
+    lines.forEach((item) => {
+      const [val1, val2] = item.split("   ");
+
+      list1.push(Number(val1));
+      list2.push(Number(val2));
+    });
+
+    list1.sort((a, b) => a - b);
+    list2.sort((a, b) => a - b);
+
+    let total = 0;
+    for (let i = 0; i < list1.length; i++) {
+      const val1 = list1[i];
+      const val2 = list2[i];
+      total += Math.abs(val2 - val1);
+    }
+
+    console.log(total);
+  });
+};
+
+getTotalDistance();
