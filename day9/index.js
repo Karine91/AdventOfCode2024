3587498b8415b7760beca86743ef76cdeb673e3b
karine <gulkarine91@gmail.com>
2024-12-15 00:10:08 +0100

    chore: finsih part 2 d9

--- day9/index.js
+++ day9/index.js
@@ -6,7 +6,7 @@ fs.readFile(path.resolve(__dirname, "input.txt"), "utf8", (err, data) => {
     console.log(err);
   } else {
     console.time("start");
-    const res = compactAmphipodDrive(data);
+    const res = compactAmphipodDrive2(data);
     console.log("Result: ", res);
     console.timeEnd("start");
   }
@@ -60,7 +60,96 @@ function compactAmphipodDrive(data) {
   return sum;
 }
 
+function compactAmphipodDrive2(data) {
+  const idsArr = [];
+  const idsMap = [];
+
+  let ind = 0;
+  for (let i = 0; i < data.length; i += 2) {
+    const file = Number(data[i]);
+    const fileId = i / 2;
+    const space = Number(data[i + 1]);
+
+    const pos = { start: ind };
+    for (let n = 0; n < file; n++) {
+      idsArr.push(fileId);
+      ind++;
+    }
+    pos.end = ind;
+    idsMap.push({ id: fileId, ...pos, length: pos.end - pos.start });
+    if (!isNaN(space)) {
+      for (let s = 0; s < space; s++) {
+        idsArr.push(".");
+        ind++;
+      }
+    }
+  }
+
+  idsMap.sort((a, b) => b.id - a.id);
+
+  //rearrange
+
+  function getSpaceLength(start, end) {
+    let endPos = start;
+    if (idsArr[start] === ".") {
+      while (idsArr[endPos] === "." && endPos < end) {
+        endPos++;
+      }
+    }
+
+    return { length: Math.abs(endPos - start), start, end: endPos };
+  }
+
+  function searchForFit(start, end, target) {
+    let lastEmptySpace = start;
+    let searching = false;
+    while (start < end) {
+      if (idsArr[start] !== ".") {
+        start++;
+        continue;
+      }
+      const spaceLength = getSpaceLength(start, end);
+
+      let p = target.start;
+      if (spaceLength.length >= target.length) {
+        while (p < target.end) {
+          idsArr[start] = idsArr[p];
+          idsArr[p] = ".";
+          start++;
+          p++;
+        }
+        return [searching ? lastEmptySpace : start, target.start];
+      } else {
+        if (!searching) {
+          searching = true;
+          lastEmptySpace = start;
+        }
+
+        start = spaceLength.end;
+      }
+    }
+
+    return [lastEmptySpace, target.start];
+  }
+
+  let p1 = 0;
+  let p2 = idsArr.length;
+  for (let file of idsMap) {
+    [p1, p2] = searchForFit(p1, p2, file);
+  }
+
+  let sum = 0;
+
+  for (let i = 0; i < idsArr.length; i++) {
+    if (idsArr[i] === ".") continue;
+
+    sum += i * Number(idsArr[i]);
+  }
+
+  return sum;
+}
+
 const testInput = "2333133121414131402";
 const testInput2 = "12345";
 
-console.log(compactAmphipodDrive(testInput));
+console.log(compactAmphipodDrive2(testInput));
